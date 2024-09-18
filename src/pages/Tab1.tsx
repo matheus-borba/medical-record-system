4ff5426dd0a04e91c61c3dea98ee8b900595b4a8
Matheus Borba <54252575+matheus-borba@users.noreply.github.com>
2024-09-18 03:08:20 -0300

    Adiciona conexao com database (#3)

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,8 +1,28 @@
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
+import { auth, db } from '../services/firebaseConfig';
+import { collection, getDocs } from "firebase/firestore";
+
 import './Tab1.css';
+import { get, ref } from 'firebase/database';
+import { useState } from 'react';
 
 const Tab1: React.FC = () => {
+
+  // let [pacientesList, setPacientesList] = useState([]);
+
+  // const getPacientes = async () => {
+  //   console.log("TESTEEEE")
+  //   const dbRef = ref(db, "pacientes");
+  //   const snapshot = await get(dbRef);
+  //   if (snapshot.exists()) {
+  //     setPacientesList(Object.values(snapshot.val()));
+  //   }
+  //   console.log(pacientesList);
+  // }
+
+  // getPacientes();
+
   return (
     <IonPage>
       <IonHeader>
--- src/services/firebaseConfig.ts
+++ src/services/firebaseConfig.ts
@@ -1,9 +1,11 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBqOURlboS6T2BZPgquo5EU05er9p0SGiY",
   authDomain: "medical-record-system-2024.firebaseapp.com",
+  databaseURL: "https://medical-record-system-2024-default-rtdb.firebaseio.com",
   projectId: "medical-record-system-2024",
   storageBucket: "medical-record-system-2024.appspot.com",
   messagingSenderId: "787223073260",
@@ -13,4 +15,5 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 
+export const db = getDatabase(app);
 export const auth = getAuth(app);
